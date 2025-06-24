9cb955cc6f768773479bf67d656a818ddbc95d4c
Clara Echeverría <cecheverria@correo.um.edu.uy>
2025-06-24 18:15:11 -0300

    feat: add data method

--- infra/dynamo.tf
+++ infra/dynamo.tf
@@ -2,11 +2,11 @@ resource "aws_dynamodb_table" "flavios_records" {
   name         = "flaviosRecords"
   billing_mode = "PAY_PER_REQUEST"
 
-  hash_key  = "productID"   # partition key
+  hash_key  = "recordID"   # partition key
   range_key = "timestamp"   # sort key; store ISO-8601 or epoch
 
   attribute {
-    name = "productID"
+    name = "recordID"
     type = "S"
   }
   attribute {
